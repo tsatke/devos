7a01ff6046238e8c149811f5a23ed40831752c64
tsatke <tim.satke@gmail.com>
2023-05-02 17:45:21 +0200

    add differentiation between uefi and bios
    The "mode" can be set and accessed via cargo's features.

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -16,6 +16,11 @@ pub mod mem;
 pub mod process;
 pub mod qemu;
 
+#[cfg(not(any(feature = "bios", feature = "uefi")))]
+compile_error!("You must enable either the bios or uefi feature");
+#[cfg(all(feature = "bios", feature = "uefi"))]
+compile_error!("You must enable either the bios or uefi feature, not both");
+
 pub fn kernel_init(boot_info: &'static mut BootInfo) {
     gdt::init();
     idt::init();
