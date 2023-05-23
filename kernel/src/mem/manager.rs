c52e21b84f76406b52568008b8eb8c072a444dfb
tsatke <tsatke@deloitte.com>
2023-05-23 08:21:10 +0200

    switch from a bump allocator to a memory map
    For physical memory management, introduced a memory map in a two
    stage approach. This makes physical frame allocation way more efficient.

--- kernel/src/mem/manager.rs
+++ kernel/src/mem/manager.rs
@@ -1,22 +1,59 @@
 use bootloader_api::BootInfo;
 use spin::{Mutex, MutexGuard};
-use x86_64::structures::paging::{FrameAllocator, PhysFrame, Size4KiB};
+use x86_64::structures::paging::{FrameAllocator, FrameDeallocator, PhysFrame, Size4KiB};
 
-use crate::mem::physical::PhysicalFrameAllocator;
+use crate::mem::physical_stage1::TrivialPhysicalFrameAllocator;
+use crate::mem::physical_stage2::MemoryMapPhysicalFrameAllocator;
 
 static mut MEMORY_MANAGER: Option<Mutex<MemoryManager>> = None;
 
-pub(in crate::mem) fn init(boot_info: &'static BootInfo) {
-    let mm = MemoryManager {
-        alloc: PhysicalFrameAllocator::from(&boot_info.memory_regions),
-    };
+// TODO: remove the two-stage approach
+// Currently, we need this, because the address space requires an initialized stage1 allocator
+// to allocate page table frames.
+pub(in crate::mem) fn init_stage1(boot_info: &'static BootInfo) {
+    init(Allocator::Stage1(TrivialPhysicalFrameAllocator::from(
+        &boot_info.memory_regions,
+    )));
+}
+
+pub(in crate::mem) fn init_stage2(boot_info: &'static BootInfo) {
+    init(Allocator::Stage2(MemoryMapPhysicalFrameAllocator::from(
+        &boot_info.memory_regions,
+    )));
+}
+
+fn init(stage: Allocator) {
+    let mm = MemoryManager { alloc: stage };
     unsafe {
         let _ = MEMORY_MANAGER.insert(Mutex::new(mm));
     }
 }
 
+enum Allocator {
+    Stage1(TrivialPhysicalFrameAllocator),
+    Stage2(MemoryMapPhysicalFrameAllocator),
+}
+
+impl Allocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        match self {
+            Allocator::Stage1(alloc) => alloc.allocate_frame(),
+            Allocator::Stage2(alloc) => alloc.allocate_frame(),
+        }
+    }
+
+    unsafe fn deallocate_frame(&mut self, frame: PhysFrame) {
+        unsafe {
+            match self {
+                Allocator::Stage1(_) => {}
+                Allocator::Stage2(alloc) => alloc.deallocate_frame(frame),
+            }
+        }
+    }
+}
+
 pub struct MemoryManager {
-    alloc: PhysicalFrameAllocator,
+    alloc: Allocator,
 }
 
 impl MemoryManager {
@@ -34,6 +71,10 @@ impl MemoryManager {
 
     pub fn allocate_frame(&mut self) -> Option<PhysFrame> {
         self.alloc.allocate_frame()
+    }
+
+    pub fn deallocate_frame(&mut self, frame: PhysFrame) {
+        unsafe { self.alloc.deallocate_frame(frame) }
     }
 }
 
