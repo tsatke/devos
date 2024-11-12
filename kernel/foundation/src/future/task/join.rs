335a0d22d4d0bc3bcec3f488f89b8c62368c9ecf
tsatke <tim.satke@gmail.com>
2024-11-12 17:52:02 +0100

    make the join handle return an optional
    In the executor, we never drop the sender end, but if we drop the whole executor, we would panic in the join handle.

--- kernel/foundation/src/future/task/join.rs
+++ kernel/foundation/src/future/task/join.rs
@@ -17,12 +17,52 @@ impl<T> JoinHandle<T> {
 }
 
 impl<T> Future for JoinHandle<T> {
-    type Output = T;
+    type Output = Option<T>;
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         match self.receiver.as_mut().poll(cx) {
-            Poll::Ready(v) => Poll::Ready(v.unwrap()), // we must never drop the sender
+            Poll::Ready(v) => Poll::Ready(v.ok()),
             Poll::Pending => Poll::Pending,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::future::executor::{block_on, Executor, Spin};
+
+    #[test]
+    fn test_join_handle_no_panic_on_executor_drop() {
+        let exec = Executor::new();
+        let handle = exec.spawn(async { 1 }).unwrap();
+        drop(exec);
+        assert_eq!(None, block_on::<_, Spin>(handle));
+    }
+
+    #[test]
+    fn test_join_handle_await() {
+        async fn increment(a: usize) -> usize {
+            a + 1
+        }
+        async fn mul(a: usize, b: usize) -> usize {
+            a * b
+        }
+
+        let exec = Executor::new();
+        let result = exec.spawn(async {
+            let a = increment(1).await;
+            let b = increment(2).await;
+            mul(a, b).await
+        }).unwrap();
+
+        let actual_result = exec.spawn(async move {
+            let x = result.await;
+            increment(x.unwrap_or(0)).await
+        }).unwrap();
+
+        exec.run_active_tasks_to_completion();
+
+        let result = block_on::<_, Spin>(actual_result).unwrap();
+        assert_eq!(result, 7);
     }
 }
