1ee82cd88dedbf780c1537086182c61861781ade
tsatke <tim.satke@gmail.com>
2025-01-22 21:07:51 +0100

    add headless option to run without window (but with graphics devices)

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ struct Args {
         help = "Start QEMU with a GDB server listening on localhost:1234"
     )]
     debug: bool,
+    #[arg(long, help = "Run QEMU without a display")]
+    headless: bool,
 }
 
 fn main() {
@@ -44,21 +46,39 @@ continue"#
     }
 
     let mut cmd = std::process::Command::new("qemu-system-x86_64");
+    // don't reboot on triple faults
     cmd.arg("--no-reboot");
+
+    // serial comms via console - needed for log output of the kernel
     cmd.arg("-serial");
     cmd.arg("stdio");
+
+    // QEMU monitor via telnet
     cmd.arg("-monitor");
     cmd.arg("telnet::45454,server,nowait");
+
+    // start GDB server
     cmd.arg("-s");
+
     if args.debug {
+        // wait for client to connect
         cmd.arg("-S");
     }
+
+    if args.headless {
+        // run without a window, but with graphics devices attached
+        cmd.arg("-nographic");
+    }
+
+    // OVMF firmware
     cmd.arg("-drive");
     cmd.arg(format!(
         "if=pflash,unit=0,format=raw,file={OVMF_CODE},readonly=on"
     ));
     cmd.arg("-drive");
     cmd.arg(format!("if=pflash,unit=1,format=raw,file={OVMF_VARS}"));
+
+    // kernel binary
     cmd.arg("-cdrom");
     cmd.arg(BOOTABLE_ISO);
 
