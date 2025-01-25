1fcdfd55bd61c3fe30072c33b0c3b1e932aa9240
tsatke <tim.satke@gmail.com>
2025-01-25 14:19:29 +0100

    add interrupt handlers for some more exceptions, which helps debugging

--- kernel/src/arch/idt.rs
+++ kernel/src/arch/idt.rs
@@ -3,7 +3,8 @@ use alloc::boxed::Box;
 use conquer_once::spin::Lazy;
 use core::pin::Pin;
 use spin::RwLock;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::registers::control::Cr2;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 static IDT: Lazy<RwLock<Pin<Box<InterruptDescriptorTable>>>> = Lazy::new(|| {
     let mut idt = InterruptDescriptorTable::new();
@@ -15,11 +16,16 @@ static IDT: Lazy<RwLock<Pin<Box<InterruptDescriptorTable>>>> = Lazy::new(|| {
     }
     idt.general_protection_fault
         .set_handler_fn(general_protection_fault_handler);
+    idt.invalid_opcode.set_handler_fn(invalid_opcode_handler);
+    idt.invalid_tss.set_handler_fn(invalid_tss_handler);
+    idt.page_fault.set_handler_fn(page_fault_handler);
     idt.segment_not_present
         .set_handler_fn(segment_not_present_handler);
+    idt.stack_segment_fault
+        .set_handler_fn(stack_segment_fault_handler);
 
     let idt = Box::pin(idt);
-    
+
     RwLock::new(idt)
 });
 
@@ -42,9 +48,34 @@ extern "x86-interrupt" fn general_protection_fault_handler(
     panic!("EXCEPTION: GENERAL PROTECTION FAULT:\nerror code: {error_code:#X}\n{stack_frame:#?}");
 }
 
+extern "x86-interrupt" fn invalid_opcode_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: INVALID OPCODE:\n{stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn invalid_tss_handler(stack_frame: InterruptStackFrame, error_code: u64) {
+    panic!("EXCEPTION: INVALID TSS:\nerror code: {error_code:#X}\n{stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    let accessed_address = Cr2::read().ok();
+    panic!(
+        "EXCEPTION: PAGE FAULT:\naccessed address: {accessed_address:?}\nerror code: {error_code:#X}\n{stack_frame:#?}"
+    );
+}
+
 extern "x86-interrupt" fn segment_not_present_handler(
     stack_frame: InterruptStackFrame,
     error_code: u64,
 ) {
     panic!("EXCEPTION: SEGMENT NOT PRESENT:\nerror code: {error_code:#X}\n{stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn stack_segment_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    panic!("EXCEPTION: STACK SEGMENT FAULT:\nerror code: {error_code:#X}\n{stack_frame:#?}");
 }
