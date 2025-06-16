d1ac8ec6972c364fe8452d9f386f4ab2de756dde
tsatke <tim.satke@gmail.com>
2025-06-16 18:41:01 +0200

    move some code around

--- kernel/src/mcore/mtask/process/mod.rs
+++ kernel/src/mcore/mtask/process/mod.rs
@@ -1,38 +1,43 @@
+use alloc::borrow::ToOwned;
+use alloc::boxed::Box;
 use alloc::collections::BTreeMap;
-use alloc::string::String;
+use alloc::string::{String, ToString};
 use alloc::sync::Arc;
-use alloc::vec::Vec;
+use core::alloc::Layout;
 use core::ffi::c_void;
 use core::fmt::{Debug, Formatter};
+use core::ptr;
 
 use conquer_once::spin::OnceCell;
-use jiff::Timestamp;
 use kernel_elfloader::{ElfFile, ElfLoader};
 use kernel_memapi::{Allocation, Location, MemoryApi, UserAccessible};
-use kernel_vfs::path::{AbsoluteOwnedPath, AbsolutePath};
+use kernel_vfs::Stat;
+use kernel_vfs::path::{AbsoluteOwnedPath, AbsolutePath, ROOT};
 use log::debug;
 use spin::RwLock;
 use thiserror::Error;
 use virtual_memory_manager::VirtualMemoryManager;
+use x86_64::VirtAddr;
 use x86_64::registers::model_specific::FsBase;
 use x86_64::registers::rflags::RFlags;
 use x86_64::structures::idt::InterruptStackFrameValue;
-use x86_64::VirtAddr;
+use x86_64::structures::paging::{PageSize, Size4KiB};
 
-use crate::file::{vfs, OpenFileDescription};
+use crate::U64Ext;
+use crate::file::{OpenFileDescription, vfs};
 use crate::mcore::context::ExecutionContext;
 use crate::mcore::mtask::process::fd::{FdNum, FileDescriptor, FileDescriptorFlags};
 use crate::mcore::mtask::process::tree::process_tree;
-use crate::mcore::mtask::task::{Stack, StackAllocationError, StackUserAccessible};
+use crate::mcore::mtask::task::{Stack, StackAllocationError, StackUserAccessible, Task};
 use crate::mem::address_space::AddressSpace;
-use crate::mem::memapi::LowerHalfMemoryApi;
+use crate::mem::memapi::{Executable, LowerHalfAllocation, LowerHalfMemoryApi};
 
 pub mod fd;
 mod id;
 pub use id::*;
-use kernel_vfs::Stat;
 
-use crate::time::TimestampExt;
+use crate::mcore::mtask::scheduler::global::GlobalTaskQueue;
+use crate::mem::virt::{VirtualMemoryAllocator, VirtualMemoryHigherHalf};
 
 mod tree;
 
@@ -45,12 +50,120 @@ pub struct Process {
     ppid: RwLock<ProcessId>,
 
     executable_path: Option<AbsoluteOwnedPath>,
+    executable_file_data: RwLock<Option<LowerHalfAllocation<Executable>>>,
     current_working_directory: RwLock<AbsoluteOwnedPath>,
 
     address_space: Option<AddressSpace>,
     lower_half_memory: Arc<RwLock<VirtualMemoryManager>>,
 
     file_descriptors: RwLock<BTreeMap<FdNum, FileDescriptor>>,
+}
+
+impl Process {
+    pub fn root() -> &'static Arc<Process> {
+        ROOT_PROCESS.get_or_init(|| {
+            let pid = ProcessId::new();
+            let root = Arc::new(Self {
+                pid,
+                name: "root".to_string(),
+                ppid: RwLock::new(pid),
+                executable_path: None,
+                executable_file_data: RwLock::new(None),
+                current_working_directory: RwLock::new(ROOT.to_owned()),
+                address_space: None,
+                lower_half_memory: Arc::new(RwLock::new(VirtualMemoryManager::new(
+                    VirtAddr::new(0x00),
+                    0x0000_7FFF_FFFF_FFFF,
+                ))),
+                file_descriptors: RwLock::new(BTreeMap::new()),
+            });
+            process_tree().write().processes.insert(pid, root.clone());
+            root
+        })
+    }
+
+    fn create_new(
+        parent: &Arc<Process>,
+        name: String,
+        executable_path: Option<impl AsRef<AbsolutePath>>,
+    ) -> Arc<Self> {
+        let pid = ProcessId::new();
+        let parent_pid = parent.pid;
+        let address_space = AddressSpace::new();
+
+        let process = Self {
+            pid,
+            name,
+            ppid: RwLock::new(parent_pid),
+            executable_path: executable_path.map(|x| x.as_ref().to_owned()),
+            executable_file_data: RwLock::new(None),
+            current_working_directory: RwLock::new(parent.current_working_directory.read().clone()),
+            address_space: Some(address_space),
+            lower_half_memory: Arc::new(RwLock::new(VirtualMemoryManager::new(
+                VirtAddr::new(0xF000),
+                0x0000_7FFF_FFFF_0FFF,
+            ))),
+            file_descriptors: RwLock::new(BTreeMap::new()),
+        };
+
+        let res = Arc::new(process);
+        process_tree().write().processes.insert(pid, res.clone());
+        res
+    }
+
+    // TODO: add documentation
+    #[allow(clippy::missing_errors_doc)]
+    pub fn create_from_executable(
+        parent: &Arc<Process>,
+        path: impl AsRef<AbsolutePath>,
+    ) -> Result<Arc<Self>, CreateProcessError> {
+        let path = path.as_ref();
+        let process = Self::create_new(parent, path.to_string(), Some(path));
+
+        let kstack = Stack::allocate(
+            16,
+            &VirtualMemoryHigherHalf,
+            StackUserAccessible::No,
+            AddressSpace::kernel(),
+            trampoline,
+            ptr::null_mut(),
+            Task::exit,
+        )?;
+        let main_task = Task::create_with_stack(&process, kstack);
+        GlobalTaskQueue::enqueue(Box::pin(main_task));
+
+        Ok(process)
+    }
+
+    pub fn pid(&self) -> ProcessId {
+        self.pid
+    }
+
+    pub fn ppid(&self) -> ProcessId {
+        *self.ppid.read()
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn file_descriptors(&self) -> &RwLock<BTreeMap<FdNum, FileDescriptor>> {
+        &self.file_descriptors
+    }
+
+    pub fn address_space(&self) -> &AddressSpace {
+        self.address_space
+            .as_ref()
+            .unwrap_or(AddressSpace::kernel())
+    }
+
+    pub fn vmm(self: &Arc<Self>) -> impl VirtualMemoryAllocator {
+        self.lower_half_memory.clone()
+    }
+
+    pub fn current_working_directory(&self) -> &RwLock<AbsoluteOwnedPath> {
+        &self.current_working_directory
+    }
 }
 
 impl Debug for Process {
@@ -108,28 +221,32 @@ extern "C" fn trampoline(_arg: *mut c_void) {
         stat
     };
 
-    let start = Timestamp::now();
-    let mut data = Vec::with_capacity(stat.size);
-    let mut buf = [0; 4096];
+    let mut memapi = LowerHalfMemoryApi::new(current_process.clone());
+
+    let mut executable_file_allocation = memapi
+        .allocate(
+            Location::Anywhere,
+            Layout::from_size_align(stat.size, Size4KiB::SIZE.into_usize()).unwrap(),
+            UserAccessible::Yes,
+        )
+        .expect("should be able to allocate memory for executable file");
+    let buf = executable_file_allocation.as_mut();
     let mut offset = 0;
     loop {
-        let read = node.read(&mut buf, offset).expect("should be able to read");
+        let read = node
+            .read(&mut buf[offset..], offset)
+            .expect("should be able to read");
         if read == 0 {
             break;
         }
         offset += read;
-        data.extend_from_slice(&buf[..read]);
     }
-    let stop = Timestamp::now();
-    debug!(
-        "read {}KiB in {:?}",
-        data.len() / 1024,
-        stop.since(start).unwrap(),
-    );
+    let executable_file_allocation = memapi
+        .make_executable(executable_file_allocation)
+        .expect("should be able to make allocation executable");
 
-    let mut memapi = LowerHalfMemoryApi::new(current_process.clone());
-
-    let elf_file = ElfFile::try_parse(&data).expect("should be able to parse elf binary");
+    let elf_file = ElfFile::try_parse(executable_file_allocation.as_ref())
+        .expect("should be able to parse elf binary");
     let elf_image = ElfLoader::new(memapi.clone())
         .load(elf_file)
         .expect("should be able to load elf file");
@@ -169,6 +286,10 @@ extern "C" fn trampoline(_arg: *mut c_void) {
     let sel = ctx.selectors();
 
     let code_ptr = elf_file.entry(); // TODO: this needs to be computed when the elf file is relocatable
+    let _ = current_process
+        .executable_file_data
+        .write()
+        .insert(executable_file_allocation);
 
     debug!("stack_ptr: {:p}", ustack_rsp.as_ptr::<u8>());
     debug!("code_ptr: {:p}", code_ptr as *const u8);
--- kernel/src/mcore/mtask/process/tree.rs
+++ kernel/src/mcore/mtask/process/tree.rs
@@ -1,25 +1,11 @@
-use alloc::borrow::ToOwned;
-use alloc::boxed::Box;
 use alloc::collections::BTreeMap;
-use alloc::string::{String, ToString};
 use alloc::sync::Arc;
 use alloc::vec::Vec;
-use core::ptr;
 
 use conquer_once::spin::OnceCell;
-use kernel_vfs::path::{AbsoluteOwnedPath, AbsolutePath, ROOT};
 use spin::{RwLock, RwLockReadGuard, RwLockWriteGuard};
-use virtual_memory_manager::VirtualMemoryManager;
-use x86_64::VirtAddr;
 
-use crate::mcore::mtask::process;
-use crate::mcore::mtask::process::fd::{FdNum, FileDescriptor};
-use crate::mcore::mtask::process::{CreateProcessError, Process, ProcessId, ROOT_PROCESS};
-use crate::mcore::mtask::scheduler::global::GlobalTaskQueue;
-use crate::mcore::mtask::task::{Stack, StackUserAccessible, Task};
-use crate::mem::address_space::AddressSpace;
-use crate::mem::virt::{VirtualMemoryAllocator, VirtualMemoryHigherHalf};
-
+use crate::mcore::mtask::process::{Process, ProcessId};
 static PROCESS_TREE: OnceCell<RwLock<ProcessTree>> = OnceCell::uninit();
 
 pub fn process_tree() -> &'static RwLock<ProcessTree> {
@@ -68,95 +54,6 @@ impl ChildrenMut<'_> {
 }
 
 impl Process {
-    pub fn root() -> &'static Arc<Process> {
-        ROOT_PROCESS.get_or_init(|| {
-            let pid = ProcessId::new();
-            let root = Arc::new(Self {
-                pid,
-                name: "root".to_string(),
-                ppid: RwLock::new(pid),
-                executable_path: None,
-                current_working_directory: RwLock::new(ROOT.to_owned()),
-                address_space: None,
-                lower_half_memory: Arc::new(RwLock::new(VirtualMemoryManager::new(
-                    VirtAddr::new(0x00),
-                    0x0000_7FFF_FFFF_FFFF,
-                ))),
-                file_descriptors: RwLock::new(BTreeMap::new()),
-            });
-            process_tree().write().processes.insert(pid, root.clone());
-            root
-        })
-    }
-
-    fn create_new(
-        parent: &Arc<Process>,
-        name: String,
-        executable_path: Option<impl AsRef<AbsolutePath>>,
-    ) -> Arc<Self> {
-        let pid = ProcessId::new();
-        let parent_pid = parent.pid;
-        let address_space = AddressSpace::new();
-
-        let process = Self {
-            pid,
-            name,
-            ppid: RwLock::new(parent_pid),
-            executable_path: executable_path.map(|x| x.as_ref().to_owned()),
-            current_working_directory: RwLock::new(parent.current_working_directory.read().clone()),
-            address_space: Some(address_space),
-            lower_half_memory: Arc::new(RwLock::new(VirtualMemoryManager::new(
-                VirtAddr::new(0xF000),
-                0x0000_7FFF_FFFF_0FFF,
-            ))),
-            file_descriptors: RwLock::new(BTreeMap::new()),
-        };
-
-        let res = Arc::new(process);
-        process_tree().write().processes.insert(pid, res.clone());
-        res
-    }
-
-    // TODO: add documentation
-    #[allow(clippy::missing_errors_doc)]
-    pub fn create_from_executable(
-        parent: &Arc<Process>,
-        path: impl AsRef<AbsolutePath>,
-    ) -> Result<Arc<Self>, CreateProcessError> {
-        let path = path.as_ref();
-        let process = Self::create_new(parent, path.to_string(), Some(path));
-
-        let kstack = Stack::allocate(
-            16,
-            &VirtualMemoryHigherHalf,
-            StackUserAccessible::No,
-            AddressSpace::kernel(),
-            process::trampoline,
-            ptr::null_mut(),
-            Task::exit,
-        )?;
-        let main_task = Task::create_with_stack(&process, kstack);
-        GlobalTaskQueue::enqueue(Box::pin(main_task));
-
-        Ok(process)
-    }
-
-    pub fn pid(&self) -> ProcessId {
-        self.pid
-    }
-
-    pub fn ppid(&self) -> ProcessId {
-        *self.ppid.read()
-    }
-
-    pub fn name(&self) -> &str {
-        &self.name
-    }
-
-    pub fn file_descriptors(&self) -> &RwLock<BTreeMap<FdNum, FileDescriptor>> {
-        &self.file_descriptors
-    }
-
     #[allow(clippy::missing_panics_doc)] // this panic must not happen, so the caller shouldn't have to care about it
     pub fn parent(&self) -> Arc<Process> {
         process_tree()
@@ -181,19 +78,5 @@ impl Process {
             guard,
             pid: self.pid,
         }
-    }
-
-    pub fn address_space(&self) -> &AddressSpace {
-        self.address_space
-            .as_ref()
-            .unwrap_or(AddressSpace::kernel())
-    }
-
-    pub fn vmm(self: &Arc<Self>) -> impl VirtualMemoryAllocator {
-        self.lower_half_memory.clone()
-    }
-
-    pub fn current_working_directory(&self) -> &RwLock<AbsoluteOwnedPath> {
-        &self.current_working_directory
     }
 }
